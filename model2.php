9509ea207b91e40e0a2270bc4464912fbcd2cc2b
Ridrepp <57098638+Ridrepp@users.noreply.github.com>
2020-04-25 15:03:49 +0200

    Pridanie knižníc, prvé setnutie canvasov a grafov

--- model2.php
+++ model2.php
@@ -9,13 +9,24 @@
     <title>Model 2</title>
     <link rel="stylesheet" type="text/css" href="style.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/p5@1.0.0/lib/p5.js"></script>
+    <script>
+        function setup() {
+            let cnv = createCanvas(400, 400);
+            cnv.class("kyvadlo p5Canvas");
+            cnv.id("kyvadlo");
+        }
+        function draw() {
+            background(100);
+        }
+    </script>
+    <script src="https://cdn.plot.ly/plotly-latest.min.js"></script>
 
 </head>
 
 <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <a class="navbar-brand" href="index.php"><?php echo $lang["navMainSite"]?><span class="sr-only">(current)</span></a>
-
 
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto">
@@ -32,12 +43,23 @@
         </div>
     </nav>
 
+    <br>
+
+    <div id="tester" style="width:600px;height:250px;"></div>
+
+    <script>
+        TESTER = document.getElementById('tester');
+        Plotly.newPlot( TESTER, [{
+            x: [1, 2, 3, 4, 5],
+            y: [1, 2, 4, 8, 16] }], {
+            margin: { t: 0 } } );
+    </script>
+
+
     <div class="footer bg-dark">
         <a href="model2.php?lang=sk"><?php echo $lang["lang_sk"]?></a>
         | <a href="model2.php?lang=en"><?php echo $lang["lang_en"]?></a>
     </div>
-
-
 
 </body>
 
