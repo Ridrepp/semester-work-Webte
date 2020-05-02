d6af2de81b2b1f55b554f36373102ebaf5d168a5
Ridrepp <57098638+Ridrepp@users.noreply.github.com>
2020-05-02 18:27:50 +0200

    Mini grafické úpravy

--- documentation.php
+++ documentation.php
@@ -12,6 +12,8 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
     <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/notify/0.4.2/notify.min.js"></script>
+
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
     <script src="js/mailScript.js"></script>
 </head>
 
--- mail.php
+++ mail.php
@@ -3,7 +3,7 @@ include "config.php";
 $headers = "Content-Type: text/html; charset=UTF-8";
 if (isset($_POST["email"])){
     $email = $_POST["email"];
-    $msg = "Počet použití pre jednotlivé stránky:<br>";
+    $msg = "Využitie jednotlivých stránok:<br>";
     try {
         $conn = new PDO("mysql:host=localhost;dbname=semestralneZadanie;charset=utf8", $username, $password);
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
@@ -13,7 +13,7 @@ if (isset($_POST["email"])){
     $query = "SELECT * FROM `visits`";
     $result = $conn->query($query);
     while($row = $result->fetch()){
-        $msg .= $row[1] . ": " . $row[2] . " použití<br>";
+        $msg .= $row[1] . ": " . $row[2] . " x<br>";
     }
     $success = mail($email, "Štatistika - Team: Martin Michale, Martin Domorák, Patrik Majtán", $msg ,$headers);
     if ($success) {
